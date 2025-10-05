f63d9701e3ae6e1780b8d389e69dc508a4b05998
keertigururaj2006 <keertigururaj2006@gmail.com>
2025-10-05 13:01:55 +0530

    Update program4.py

--- program4.py
+++ program4.py
@@ -2,5 +2,9 @@ a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 sum_result = a + b
 sub_result = a - b
+mult_result = a * b
+div_result = a / b
 print("The sum of", a, "and", b, "is:", sum_result)
 print("The difference between", a, "and", b, "is:", sub_result)
+print("The multiplication of", a, "and", b, "is:", mult_result)
+print("The difference between", a, "and", b, "is:", div_result)
