c3f41e6e6c6c95afe193ce378ab833581c53a353
keertigururaj2006 <keertigururaj2006@gmail.com>
2025-10-05 12:42:01 +0530

    Create program4.py

---
+++ program4.py
@@ -0,0 +1,6 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+sum_result = a + b
+sub_result = a - b
+print("The sum of", a, "and", b, "is:", sum_result)
+print("The difference between", a, "and", b, "is:", sub_result)
